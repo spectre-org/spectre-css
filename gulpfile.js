2bf00bdff3f1cd7bc4bfce577632829ab87626bb
Dave Stewart <dave@davestewart.co.uk>
2023-10-13 09:52:44 +0100

    Update package, build and changelog

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
-const gulp = require("gulp");
-const sass = require('gulp-sass')(require('sass'));
-const cleancss = require('gulp-clean-css');
-const csscomb = require('gulp-csscomb');
-const rename = require('gulp-rename');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const cleancss = require('gulp-clean-css')
+const csscomb = require('gulp-csscomb')
+const rename = require('gulp-rename')
+const autoprefixer = require('gulp-autoprefixer')
 
-function build() {
+function build () {
   return gulp
     .src('./src/*.scss')
-    .pipe(sass({style: 'compact', precision: 10})
-      .on('error', sass.logError)
+    .pipe(sass({ style: 'compact', precision: 10 })
+      .on('error', sass.logError),
     )
     .pipe(autoprefixer())
     .pipe(csscomb())
     .pipe(gulp.dest('./dist'))
     .pipe(cleancss())
     .pipe(rename({
-      suffix: '.min'
+      suffix: '.min',
     }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
 }
 
-function watch() {
-  gulp.watch('./**/*.scss', build);
+function watch () {
+  gulp.watch('./**/*.scss', build)
 }
 
-exports.watch = watch;
-exports.build = build;
-exports.default = build;
+exports.watch = watch
+exports.build = build
+exports.default = build
