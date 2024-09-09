d311125d48b27bec53c8797e6b188669eedb5c07
Dave Stewart <dev@davestewart.co.uk>
2024-09-09 19:51:24 +0100

    Fix CSS file banner version

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
-import gulp from 'gulp';
-import * as dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-import cleancss from 'gulp-clean-css';
-import csscomb from 'gulp-csscomb';
-import rename from 'gulp-rename';
-import autoprefixer from 'gulp-autoprefixer';
-const sass = gulpSass(dartSass);
+import gulp from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+import cleancss from 'gulp-clean-css'
+import csscomb from 'gulp-csscomb'
+import rename from 'gulp-rename'
+import replace from 'gulp-replace'
+import autoprefixer from 'gulp-autoprefixer'
+import { readFileSync } from 'fs';
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'))
+const sass = gulpSass(dartSass)
 
 export function build () {
   return gulp
     .src('./src/*.scss')
+    .pipe(replace(/%VERSION%/g, `v${pkg.version}`))
     .pipe(sass({ style: 'compact', precision: 10 })
-      .on('error', sass.logError),
+      .on('error', sass.logError)
     )
     .pipe(autoprefixer())
     .pipe(csscomb())
     .pipe(gulp.dest('./dist'))
     .pipe(cleancss())
     .pipe(rename({
-      suffix: '.min',
+      suffix: '.min'
     }))
     .pipe(gulp.dest('./dist'))
 }
