c42cad0d23a77f61bcbc863dad6cbaeffbf28fd8
Dave Stewart <dev@davestewart.co.uk>
2024-09-16 11:18:15 +0100

    Finalise prefixing and namespacing

--- gulpfile.js
+++ gulpfile.js
@@ -6,94 +6,22 @@ import cleancss from 'gulp-clean-css'
 import csscomb from 'gulp-csscomb'
 import rename from 'gulp-rename'
 import replace from 'gulp-replace'
+import gulpIf from 'gulp-if'
 import autoprefixer from 'gulp-autoprefixer'
-import prefixer from 'postcss-prefix-selector'
+import prefixSelector from 'postcss-prefix-selector'
+import prefixer from 'postcss-prefixer'
 import postcss from 'gulp-postcss'
 import { readFileSync } from 'fs'
 
-const { series } = gulp
-const pkg = JSON.parse(readFileSync('./package.json', 'utf8'))
 const sass = gulpSass(dartSass)
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'))
 
 const DIST_PATH = './dist'
 const DOCS_PATH = '../spectre-docs/.vitepress/theme/styles/spectre'
 
-
 // ---------------------------------------------------------------------------------------------------------------------
-// utils
+// lib scripts
 // ---------------------------------------------------------------------------------------------------------------------
-
-/**
- * Prefix and copy
- *
- * @param prefix    The CSS prefix to render
- * @param output    The output folder, defaults to './dist/prefixed'
- * @param names     The names of files to prefix, defaults to "*"
- * @param minOnly   Output minified files only, defaults to false
- *
- * @example
- *
- *  // everything to dist/prefixed
- *  prefix('main')
- *
- *  // spectre.css and spectre-exp.css only to dist/prefixed
- *  prefix('main', undefined, ['spectre', 'spectre-exp']
- *
- *  // spectre.css, minified only to parent docs project
- *  prefix('main', '../docs/styles', 'spectre.css', true)
- */
-export function prefix (prefix, output = `${DIST_PATH}/prefixed`, names = '*', minOnly = false) {
-  // variables
-  // BUG: cannot get gulp.src() to ignore .css.min files with '!*.min.css' pattern
-  names = names === '*'
-    ? ['spectre', 'spectre-exp', 'spectre-icons']
-    : typeof names === 'string'
-      ? [names]
-      : names
-  const include = `${DIST_PATH}/{${names.join(',')}}.css`
-
-  // do it
-  const prefixed = gulp
-    .src(include)
-    .pipe(postcss([
-      prefixer({
-        prefix,
-        exclude: [/\bhtml\S*/, /\bbody\S*/]
-      })
-    ]))
-
-  // tasks
-  const tasks = []
-  if (!minOnly) {
-    tasks.push(() => prefixed.pipe(gulp.dest(output)))
-  }
-  tasks.push(() => {
-    return prefixed
-      .pipe(cleancss())
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(gulp.dest(output))
-  })
-
-  // run
-  return series(tasks)()
-}
-
-// ---------------------------------------------------------------------------------------------------------------------
-// scripts
-// ---------------------------------------------------------------------------------------------------------------------
-
-/**
- * Build and watch in development
- */
-export function watch () {
-  const tasks = [build]
-  gulp.watch('./**/*.scss', build)
-  if (Fs.existsSync(DOCS_PATH)) {
-    gulp.watch(`${DIST_PATH}/spectre.css`, buildDocs)
-    tasks.push(buildDocs)
-  }
-  series(tasks)()
-}
 
 /**
  * Build for distribution
@@ -113,10 +41,113 @@ export function build () {
     .pipe(gulp.dest(DIST_PATH))
 }
 
+/**
+ * Watch and rebuild for development
+ */
+export function watch () {
+  return gulp.watch('./**/*.scss', { ignoreInitial: false }, build)
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// docs scripts
+// ---------------------------------------------------------------------------------------------------------------------
 
 /**
- * Build and for documentation
+ * Build and save docs stylesheets
  */
 export function buildDocs () {
-  return prefix('.vp-doc', DOCS_PATH)
+  const options = { output: DOCS_PATH, minOnly: true }
+  isolate('namespace', '.vp-doc', options)
+}
+
+/**
+ * Watch and rebuild docs stylesheets for development
+ */
+export function watchDocs () {
+  return gulp.watch(`${DIST_PATH}/spectre.css`, { ignoreInitial: false }, buildDocs)
+}
+
+/**
+ * Build and watch main lib, then namespace and save docs stylesheet to parallel docs repo
+ */
+export const docs = Fs.existsSync(DOCS_PATH)
+  ? gulp.parallel(watch, watchDocs)
+  : function () {
+    console.log(`
+The "spectre-docs" folder / repo was not found.
+
+To build only "spectre-css" run "watch" or "build".
+`)
+    return Promise.resolve()
+  }
+
+// ---------------------------------------------------------------------------------------------------------------------
+// helpers
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Isolate Options
+ *
+ * @typedef   {Object}            IsolateOptions
+ * @property  {string|string[]}   [names]       The names of files to prefix, defaults to "*" (all)
+ * @property  {string}            [output]      The output folder, defaults to './dist' (overwrites files)
+ * @property  {boolean}           [minOnly]     Output minified files only, defaults to false
+ */
+
+/**
+ * Utility function to isolate classes and save files
+ *
+ * @param {'prefix'|'namespace'}  type        The operation to run
+ * @param {string}                value       The string to use in the operation
+ * @param {IsolateOptions}        [options]   Options to customise the isolation
+ *
+ * @example
+ *
+ *  // build all files, namespace to main, and save to dist/isolated
+ *  isolate('namespace', 'main')
+ *
+ *  // build only main and exp, namespace to .docs, and save to dist/isolated
+ *  isolate('namespace', '.docs', { names: ['spectre', 'spectre-exp'] })
+ *
+ *  // build all files, prefix with `s-`, save minified files only to
+ *  isolate('prefix', 's-', { output: '../docs/styles', minOnly: true })
+ */
+function isolate (type, value, options = {}) {
+  // defaults
+  options = Object.assign({
+    names: '*',
+    output: `${DIST_PATH}/${type}`,
+    minOnly: false
+  }, options)
+
+  // options
+  let { names, output, minOnly } = options
+
+  // paths
+  // BUG: cannot get gulp.src() to ignore .css.min files with '!*.min.css' pattern so hardcoding names 😕
+  names = names === '*'
+    ? ['spectre', 'spectre-exp', 'spectre-icons']
+    : typeof names === 'string'
+      ? [names]
+      : names
+  const include = `${DIST_PATH}/{${names.join(',')}}.css`
+
+  // setup
+  const exclude = [/\b:root\b/, /\bhtml\S*/, /\bbody\S*/]
+  const plugin = type === 'namespace'
+    ? prefixSelector({ prefix: value, exclude })
+    : prefixer({ prefix: value.replace(/^\./, ''), ignore: exclude })
+
+  // task
+  return gulp
+    .src(include)
+    .pipe(postcss([plugin]))
+
+    // output
+    .pipe(gulpIf(!minOnly, gulp.dest(output)))
+
+    // minified
+    .pipe(cleancss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(output))
 }
